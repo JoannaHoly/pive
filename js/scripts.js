ad992f6538b2cee4fa213465e504427279bc42eb
Hołka <JoannaHoly@users.noreply.github.com>
2022-03-27 21:47:50 +0200

    Główne skrypty strony
    Dodano komentarz

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(function(){
 	$('.wp-block-image').click (function(){
 		let link = $(this).find('a').attr('href');
 		
+		/* If image link goes directly to img src, open it in new window, not in iframe */
 		if (link.endsWith ('.jpg') || link.endsWith ('.jpeg') || link.endsWith ('.png') || link.endsWith ('.webp') || link.endsWith ('.gif')) {
 			window.open(link);
 		} else {
