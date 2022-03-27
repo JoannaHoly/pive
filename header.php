fb46b0f871df4931bdce09aa6ea0199f0c91b214
Hołka <JoannaHoly@users.noreply.github.com>
2022-03-27 23:43:19 +0200

    Plik nagłówka strony
    Poprawiono wyświetlanie na urządzeniach mobilnych

--- header.php
+++ header.php
@@ -15,7 +15,7 @@ $post_type = get_post_type( $post->ID );
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo ('charset'); ?>">
-	<meta name="viewport" content="width=device-width, minimum-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="author" content="Joanna Hoły">
 	<meta name="generator" content="Codeanywhere">
 	<title><?php bloginfo('name'); ?></title>
